0fa44939eb8adca80afb5417f02e754f320c99d4
graycadeau <g.zawadi.w@gmail.com>
2018-11-28 11:17:02 +0300

    chore(setup): setup react router
    -setup react router version 4(react-router-dom)
    -create dummy route for login
    -create dummy route for "/"
    -create components and views file structure
    
    [chore #161255310]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
+import Index from "./components/Home";
+import Login from "./components/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login/">Login</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route exact path="/" component={Index} />
+          <Route path="/login/" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
