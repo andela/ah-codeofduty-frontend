8f2a48d6bebb5c5b9ee308a45d5a776a7dfca638
graycadeau <g.zawadi.w@gmail.com>
2018-11-28 21:38:56 +0300

    chore(setup): setup react-router
    -fix build error
    
    [chore #161255310]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./App.css";
-import Index from "./components/Home";
-import Login from "./components/Login";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.scss';
+import Index from './components/Home';
+import Login from './components/Login';
 
 class App extends Component {
   render() {
