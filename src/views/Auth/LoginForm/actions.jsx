586fbc8adeb2ef42baa05603b9d1c2545383130b
graycadeau <g.zawadi.w.gmail.com>
2018-12-19 10:06:15 +0300

    (feature #161255325): Tagging
    -fix loginUser
    
    [Feature #161255325]

--- src/views/Auth/LoginForm/actions.jsx
+++ src/views/Auth/LoginForm/actions.jsx
@@ -1,30 +1,31 @@
-import sendLoginRequest from "./thunk";
-import * as actionTypes from "./actionTypes";
+import sendLoginRequest from './thunk';
+import * as actionTypes from './actionTypes';
 
 export const setLoginPending = () => ({
-  type: actionTypes.LOGIN_PENDING
+  type: actionTypes.LOGIN_PENDING,
 });
 
 export const setLoginSuccess = user => ({
   type: actionTypes.LOGIN_SUCCESS,
-  user
+  user,
 });
 
 export const setLoginError = loginError => ({
   type: actionTypes.LOGIN_ERROR,
   isLoginError: true,
-  loginError
+  loginError,
 });
 
-export const login = ({ email, password }) => dispatch => {
+export const login = ({ email, password }) => (dispatch) => {
   dispatch(setLoginPending(true));
   sendLoginRequest({ email, password })
     .then(({ data }) => {
       dispatch(setLoginSuccess(data.user));
       // eslint ignore next-line
-      localStorage.setItem("user", JSON.stringify(data.user));
+      localStorage.setItem('user', JSON.stringify(data.user));
+      window.location.assign('/');
     })
-    .catch(err => {
+    .catch((err) => {
       dispatch(setLoginPending(false));
       dispatch(setLoginError(err.response));
     });
