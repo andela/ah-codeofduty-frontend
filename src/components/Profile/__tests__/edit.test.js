44f17a68abeb2b82ea81c8801202c45b84f292b3
maxwellgithinji <maxwellgithinji@gmail.com>
2019-01-04 14:08:50 +0300

    Feature: User should be able to report an article - ensure to increase test coverage [Feature #161255334]

--- src/components/Profile/__tests__/edit.test.js
+++ src/components/Profile/__tests__/edit.test.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import configureMockStore from 'redux-mock-store';
+import ReactDOM from 'react-dom';
 import { EditTest } from '../Edit/Edit';
 
-describe('ArticleForm component', () => {
+jest.mock('react-router-dom');
+
+describe('Edit component', () => {
   const mockStore = configureMockStore();
   let wrapper;
   const props = {
-    profile: {},
+    profile: { surname: 'name', last_name: 'mane', bio: 'yaba' },
+    showUpload: jest.fn(),
   };
+
+  const getEvent = () => ({
+    preventDefault: jest.fn(),
+  });
 
   beforeEach(() => {
     mockStore({});
-    wrapper = shallow(<EditTest {...props}/>);
+    wrapper = shallow(<EditTest {...props} />);
   });
 
   it('renders without crashing', () => {
     shallow(<EditTest {...props} />);
+  });
+
+  it('should show upload', () => {
+    wrapper.instance().showUpload(getEvent());
+    expect(wrapper.state().username).toEqual('');
+  });
+
+  it('should render div', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<EditTest {...props}/>, div);
+    ReactDOM.unmountComponentAtNode(div);
   });
 });
